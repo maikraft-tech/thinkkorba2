2b2b358f2785935c6729cbf928a6a0bd2bad42cc
Korkor <rejoicehormeku@gmail.com>
2019-09-03 19:07:38 +0000

    control

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -322,15 +322,7 @@
                                 </div>
                             </div>
                         </div>
-                        <!-- Controls -->
-                        <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
-                            <span class="glyphicon glyphicon-chevron-left" aria-hidden="true" style="font-size:40px;"></span>
-                            <span class="sr-only">Previous</span>
-                        </a>
-                        <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
-                            <span class="glyphicon glyphicon-chevron-right" aria-hidden="true" style="font-size:40px;"></span>
-                            <span class="sr-only">Next</span>
-                        </a>
+                       
                     </div>
                 </div>
             </div>
