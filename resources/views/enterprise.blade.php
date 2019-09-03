10af6d0f986d409d177094609f9860929d9344a7
Korkor <rejoicehormeku@gmail.com>
2019-09-03 16:11:44 +0000

    commit

--- resources/views/enterprise.blade.php
+++ resources/views/enterprise.blade.php
@@ -14,7 +14,7 @@
         <p>
             Korba enterprise creates the possibility for institutions to make bulk payments.
         </p>
-        <p>This process involves debiting one account and crediting multiple recipients on different financial platforms such as banks, rural banks, savings and loans and any mobile money wallet with just a click of a button. </p>
+        <p>This process involves debiting one account and crediting multiple recipients simultaneously on different financial platforms such as banks, rural banks, savings and loans and any mobile money wallet with just a click of a button. </p>
         <p>This service is highly beneficial for the payment of insurance claims, salaries, loan disbursements, donations etc.</p>
     </div>
   
