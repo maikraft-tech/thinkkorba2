429ac7b6e9dc1bb0e0d1de4b674c9dac840a40b6
Korkor <rejoicehormeku@gmail.com>
2019-09-03 11:49:48 +0000

    commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -284,7 +284,7 @@
                                 </div>
                                 <div class="col-md-2 col-xs-12">
                                     <div class="cbrand_item m-bottom-2 img">
-                                        <img src="/img/client/Surfline.png" alt="Surfline" />
+                                        <img src="/img/client/Surfline.png" alt="Surfline" style="min-height: 80px; max-height: 80px; width:100%" />
                                     </div>
                                 </div>
                                 <div class="col-md-2 col-xs-12">
