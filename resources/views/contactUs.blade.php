56d20cf4f760a6f218559d79ba43fa0dc97ac458
Korkor <rejoicehormeku@gmail.com>
2019-09-05 14:39:10 +0000

    gh

--- resources/views/contactUs.blade.php
+++ resources/views/contactUs.blade.php
@@ -10,72 +10,80 @@
         </div>  
     </div>
     
-    <div class="container" style="padding-top: 30px; position: relative;">
+    <div class="content-box-md">
+        <div class="container">
             <div class="row">
-                
-                <div class="col-md-6">
-                    <div id="contact-right">
-                        <form action="{{ url('contactUs') }}" method="POST">
-                            <h3>Leave a Message</h3>
-                            <p>Please fill out the form and we will get in touch soon</p>
-                            <div class="row">
-                            <div class="col-md-6 col-sm-6">
-                                <div class="form-group">
-                                <input type="text" name="username" class="form-control" id="name" placeholder="Enter your name here">
-                                </div>
-                            </div>
-                            <div class="col-md-6 col-sm-6">
-                                <div class="form-group">
-                                <input type="email" name="email" class="form-control" id="email" placeholder="Enter your email here">
-                                </div>
-                            </div>
-
-                            <div class="form-group">
-                                <textarea class="form-control" name="message" id="message" placeholder="Message goes here"></textarea>
-                            </div>
-                            <div id="submit-btn">
-                                <a href="#" class="btn btn-general" role="button" title="submit" class="background-color:#444141ed" type="submit">Submit</a>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-                <div class="col-md-6"> 
+                <div class="col-md-6 col-sm-6 col-xs-12"> 
                     <!-- contact left side --><br>
                     <div id="contact-left">
-                        <h3 style="margin-left: 40px;">Need help?</h3>
-                        <div id="offices">
-                                    <div>
-                                        <ul style="list-style: none; margin-left: 40px;">
-                                            <li >
-                                                <h5 style="color: red; font-weight: 1; padding-bottom: 0;"><b>Location</b></h5>
-                                                <span>1st Floor,Fodai Plaza,
-                                                <br>Community 18 Junction
-                                                <br>Baatsona, Accra</span>
-                                            </li>
-                                            <li>
-                                                <h5 style="color: red; font-weight: 1; padding-bottom: 0;"><b>Phone</b></h5>
-                                                <!-- <i class="fa fa-mobile"></i> -->
-                                                <span>(+233) 2040 4010</span>
-                                            </li>
-                                            <li>
-                                                    <h5 style="color: red; font-weight: 1; padding-bottom: 0;"><b>Mailing Address</b></h5>
-                                                <span>P.O.Box GP 18376,
-                                                <br>Accra,Ghana.</span>
-                                            </li>
-                                            
-                                        </ul>
+                            <form action="{{ url('contactUs') }}" method="POST">
+                                    <h3>Leave a Message</h3>
+                                    <p>Please fill out the form and we will get in touch soon</p>
+                                    <div class="row">
+                                    <div class="col-md-6 col-sm-6">
+                                        <div class="form-group">
+                                        <input type="text" name="username" class="form-control" id="name" placeholder="Enter your name here">
+                                        </div>
                                     </div>
-                                
-                        </div>
+                                    <div class="col-md-6 col-sm-6">
+                                        <div class="form-group">
+                                        <input type="email" name="email" class="form-control" id="email" placeholder="Enter your email here">
+                                        </div>
+                                    </div>
+        
+                                    <div class="form-group">
+                                        <textarea class="form-control" name="message" id="message" placeholder="Message goes here"></textarea>
+                                    </div>
+                                    <div id="submit-btn">
+                                        <a href="#" class="btn btn-general" role="button" title="submit" class="background-color:#444141ed" type="submit">Submit</a>
+                                    </div>
+                                </form>
                     </div>
                 </div>
-            </div>  
+                <div class="col-md-6 col-sm-6 col-xs-12">
+                    <div id="contact-right">
+                            <h3 style="margin-left: 40px;">Need help?</h3>
+                            <div id="offices">
+                                <div class="row">
+                                    <div class="col-md-6">
+                                        <div>
+    
+                                            <ul style="list-style: none; margin-left: 40px;">
+                                                <li >
+                                                    <h5 style="color: red; font-weight: 1; padding-bottom: 0;"><b>Location</b></h5>
+                                                    <span>1st Floor,Fodai Plaza,
+                                                    <br>Community 18 Junction
+                                                    <br>Baatsona, Accra</span>
+                                                </li>
+                                                <li>
+                                                    <h5 style="color: red; font-weight: 1; padding-bottom: 0;"><b>Phone</b></h5>
+                                                    <!-- <i class="fa fa-mobile"></i> -->
+                                                    <span>(+233) 2040 4010</span>
+                                                </li>
+                                                <li>
+                                                        <h5 style="color: red; font-weight: 1; padding-bottom: 0;"><b>Mailing Address</b></h5>
+                                                    <span>P.O.Box GP 18376,
+                                                    <br>Accra,Ghana.</span>
+                                                </li>
+                                                
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                    </div>
+                </div>
+            </div> 
 
-        {{-- <div class="container-fluid">
-           
-                
             
-        </div> --}}
+                
+        </div>
+
+        <div class="container-fluid">
+           
+                {!! $map['html'] !!}
+            
+        </div>
     </div>
 </section>
 
