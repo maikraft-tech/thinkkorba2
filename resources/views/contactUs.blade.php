13e4809e16b22ea225c44313b7b5415eae513cc7
Korkor <rejoicehormeku@gmail.com>
2019-09-05 14:53:37 +0000

    com

--- resources/views/contactUs.blade.php
+++ resources/views/contactUs.blade.php
@@ -14,7 +14,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-3"> 
-                    <!-- contact left side --><br>
+                    <!-- contact left side -->
                     <div id="contact-left">
                             <form action="{{ url('contactUs') }}" method="POST">
                                     <h3>Leave a Message</h3>
@@ -67,32 +67,32 @@
                     </div>
                 </div>
                 <div class="col-md-3"> 
-                        <!-- contact left side --><br>
-                        <div id="contact-left">
-                                <form action="{{ url('contactUs') }}" method="POST">
-                                        <h3>Leave a Message</h3>
-                                        <p>Please fill out the form and we will get in touch soon</p>
-                                        <div class="row">
-                                        <div class="col-md-6 col-sm-6">
-                                            <div class="form-group">
-                                            <input type="text" name="username" class="form-control" id="name" placeholder="Enter your name here">
-                                            </div>
-                                        </div>
-                                        <div class="col-md-6 col-sm-6">
-                                            <div class="form-group">
-                                            <input type="email" name="email" class="form-control" id="email" placeholder="Enter your email here">
-                                            </div>
-                                        </div>
-            
+                    <!-- contact left side -->
+                    <div id="contact-left">
+                            <form action="{{ url('contactUs') }}" method="POST">
+                                    <h3>Leave a Message</h3>
+                                    <p>Please fill out the form and we will get in touch soon</p>
+                                    <div class="row">
+                                    <div class="col-md-6 col-sm-6">
                                         <div class="form-group">
-                                            <textarea class="form-control" name="message" id="message" placeholder="Message goes here"></textarea>
+                                        <input type="text" name="username" class="form-control" id="name" placeholder="Enter your name here">
                                         </div>
-                                        <div id="submit-btn">
-                                            <a href="#" class="btn btn-general" role="button" title="submit" class="background-color:#444141ed" type="submit">Submit</a>
+                                    </div>
+                                    <div class="col-md-6 col-sm-6">
+                                        <div class="form-group">
+                                        <input type="email" name="email" class="form-control" id="email" placeholder="Enter your email here">
                                         </div>
-                                    </form>
-                        </div>
+                                    </div>
+        
+                                    <div class="form-group">
+                                        <textarea class="form-control" name="message" id="message" placeholder="Message goes here"></textarea>
+                                    </div>
+                                    <div id="submit-btn">
+                                        <a href="#" class="btn btn-general" role="button" title="submit" class="background-color:#444141ed" type="submit">Submit</a>
+                                    </div>
+                                </form>
                     </div>
+                </div>
             </div> 
 
             
