89bb0d7b6377316e8ef70d52126b784fdc6e22b7
Korkor <rejoicehormeku@gmail.com>
2019-09-05 17:38:33 +0000

    cikj

--- resources/views/layouts/custom.blade.php
+++ resources/views/layouts/custom.blade.php
@@ -71,6 +71,21 @@
     <script type="text/javascript" src="js/script1.js"></script>
     @stack('scripts')
 
+
+    <script>
+            const $menu = $('.berrymenu');
+        
+            $(document).mouseup(e => {
+                if (!$menu.is(e.target) // if the target of the click isn't the container...
+                && $menu.has(e.target).length === 0) // ... nor a descendant of the container
+                {
+                    $menu.removeClass('is-active');
+                }
+                });
+        
+            $('.toggle').on('click', () => {
+                $menu.toggleClass('is-active');
+        });</script>
 <!-- this is the chat plugin -->
     <!--Start of Tawk.to Script-->
     <script type="text/javascript">
