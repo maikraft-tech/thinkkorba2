babb63f3bad3059fbd37efc977f96e2631a11de6
Korkor <rejoicehormeku@gmail.com>
2019-09-05 15:52:46 +0000

    commit

--- resources/views/contactUs.blade.php
+++ resources/views/contactUs.blade.php
@@ -45,28 +45,6 @@
                                 </div>
                             </div>
                         </div>
-                        <form action="{{ url('contactUs') }}" method="POST">
-                            <h3>Leave a Message</h3>
-                            <p>Please fill out the form and we will get in touch soon</p>
-                            <div class="row">
-                            <div class="col-md-6 col-sm-6">
-                                <div class="form-group">
-                                <input type="text" name="username" class="form-control" id="name" placeholder="Enter your name here">
-                                </div>
-                            </div>
-                            <div class="col-md-6 col-sm-6">
-                                <div class="form-group">
-                                <input type="email" name="email" class="form-control" id="email" placeholder="Enter your email here">
-                                </div>
-                            </div>
-
-                            <div class="form-group">
-                                <textarea class="form-control" name="message" id="message" placeholder="Message goes here"></textarea>
-                            </div>
-                            <div id="submit-btn">
-                                <a href="#" class="btn btn-general" role="button" title="submit" class="background-color:#444141ed" type="submit">Submit</a>
-                            </div>
-                        </form>
                     </div>
                 </div>
                 <div class="col-md-6 col-sm-6 col-xs-12">
