0a4e4439899c8c57d85a2c66f409d2b20c89bb0c
Korkor <rejoicehormeku@gmail.com>
2019-09-05 17:37:25 +0000

    commit

--- resources/views/inc/sidemenu.blade.php
+++ resources/views/inc/sidemenu.blade.php
@@ -95,23 +95,6 @@
     </div>
 </div>
 
-
-
-
-<script>
-    const $menu = $('#menu');
-
-    $(document).mouseup(e => {
-        if (!$menu.is(e.target) // if the target of the click isn't the container...
-        && $menu.has(e.target).length === 0) // ... nor a descendant of the container
-        {
-            $menu.removeClass('is-active');
-        }
-        });
-
-    $('.toggle').on('click', () => {
-        $menu.toggleClass('is-active');
-});</script>
 <!-- new menu -->
 
 <!-- <div class="hidden-panel">
