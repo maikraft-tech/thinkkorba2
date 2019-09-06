5d1092ef6add19f5f3613461d7a9ba11a0fcb092
Korkor <rejoicehormeku@gmail.com>
2019-09-06 11:18:42 +0000

    menu

--- public/js/menu.js
+++ public/js/menu.js
@@ -1,1 +1,13 @@
-$(document).click(function() { $("nav").hide(); }); $("#menu").click(function(e) { e.stopPropagation(); return false; });
+const $menu = $('.menu-bar');
+
+$(document).mouseup(e => {
+   if (!$menu.is(e.target) // if the target of the click isn't the container...
+   && $menu.has(e.target).length === 0) // ... nor a descendant of the container
+   {
+     $menu.removeClass('is-active');
+  }
+ });
+
+$('#menu').on('click', () => {
+  $menu.toggleClass('is-active');
+});
