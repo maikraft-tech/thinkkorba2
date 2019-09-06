13bf4020a054cb9c4fee43bfa358d3690d281e0e
Korkor <rejoicehormeku@gmail.com>
2019-09-06 11:44:32 +0000

    commit

--- public/js/menu.js
+++ public/js/menu.js
@@ -4,10 +4,10 @@ $(document).mouseup(e => {
    if (!$menu.is(e.target) // if the target of the click isn't the container...
    && $menu.has(e.target).length === 0) // ... nor a descendant of the container
    {
-     $menu.remove('active');
+     $menu.remove('panel-open');
   }
  });
 
 $('#menu').on('click', () => {
-  $menu.toggleClass('active');
+  $menu.toggleClass('panel-close');
 });
--- public/js/script1.js
+++ public/js/script1.js
@@ -6,10 +6,6 @@ Element.prototype.hasClass=function(className){return this.className&&new RegExp
 var menuToggle=document.querySelector('[data-js="menu-toggle"]');
 var interval=setInterval(function(){menuToggle.click()},2000);
 document.body.addEventListener('click',function(){clearInterval(interval)});
-if (menuToggle){
-menuToggle.addEventListener('click',function()
-{
-    document.body.classList.toggle('panel-open');
-    menuToggle.classList.toggle('open')});
-}
-var closePanel=document.querySelector('[data-js="hidden-panel-close"]');closePanel.addEventListener('click',function(){document.body.classList.remove('panel-open');menuToggle.classList.remove('open')})
+
+menuToggle.addEventListener('click',function(){document.body.classList.toggle('panel-open');
+menuToggle.classList.toggle('open')});var closePanel=document.querySelector('[data-js="hidden-panel-close"]');closePanel.addEventListener('click',function(){document.body.classList.remove('panel-open');menuToggle.classList.remove('open')})
