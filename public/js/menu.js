66dc04b1bcf90259ca5b49d80f1f3932632be930
Korkor <rejoicehormeku@gmail.com>
2019-09-06 11:47:44 +0000

    commit

--- public/js/menu.js
+++ public/js/menu.js
@@ -4,10 +4,11 @@ $(document).mouseup(e => {
    if (!$menu.is(e.target) // if the target of the click isn't the container...
    && $menu.has(e.target).length === 0) // ... nor a descendant of the container
    {
-     $menu.add('panel-open');
+     $menu.add('show');
   }
+  else {$menu.add('hide');}
  });
 
 $('#menu').on('click', () => {
-  $menu.toggleClass('close');
+  $menu.toggleClass('hide');
 });
