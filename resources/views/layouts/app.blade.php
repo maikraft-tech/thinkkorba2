55f9e95ef930511c34c812148b4ef1aa325a1593
Korkor <rejoicehormeku@gmail.com>
2019-09-10 11:44:58 +0000

    tawk.to plugin changed

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -79,21 +79,20 @@
     <!-- Custom js -->
     <script type="text/javascript" src="js/script.js"></script>
 
-
-<!-- this is the chat plugin -->
     <!--Start of Tawk.to Script-->
     <script type="text/javascript">
         var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
         (function(){
         var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
         s1.async=true;
-        s1.src='https://embed.tawk.to/5bdbe4a58cf925451c51c412/default';
+        s1.src='https://embed.tawk.to/5d7789e177aa790be3335394/default';
         s1.charset='UTF-8';
         s1.setAttribute('crossorigin','*');
         s0.parentNode.insertBefore(s1,s0);
         })();
-    </script>
-    <!--End of Tawk.to Script-->
+        </script>
+        <!--End of Tawk.to Script-->
+
 </body>
 
 </html>
