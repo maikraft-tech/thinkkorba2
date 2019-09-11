fa5dc8ac0a3112ea5b7f9e089d7be985c09a4819
Korkor <rejoicehormeku@gmail.com>
2019-09-11 20:14:39 +0000

    commiti

--- public/js/script.js
+++ public/js/script.js
@@ -77,7 +77,7 @@ const $menu = $('.menu-bar');
 
 $(document).mouseup(e => {
    if (!$menu.is(e.target) // if the target of the click isn't the container...
-   && $menu.has(e.target).length === 0) // ... nor a descendant of the container
+   && $menu.has(e.target).length === 1) // ... nor a descendant of the container
    {
     $("#menu").toggle('open');
   }
