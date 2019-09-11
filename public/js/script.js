de767b72f51b32726904d604122ea24f9d65d9ae
Korkor <rejoicehormeku@gmail.com>
2019-09-11 19:35:40 +0000

    removeclass menu-bar

--- public/js/script.js
+++ public/js/script.js
@@ -79,7 +79,7 @@ $(document).mouseup(e => {
    if (!$menu.is(e.target) // if the target of the click isn't the container...
    && $menu.has(e.target).length === 0) // ... nor a descendant of the container
    {
-     $menu.toggleClass('hide');
+     $menu.removeClass('menu-bar');
   }
  });
 
