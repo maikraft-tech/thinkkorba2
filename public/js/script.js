9e019c5634b5c127060df2504340d8e58409b3c0
Korkor <rejoicehormeku@gmail.com>
2019-09-11 19:33:14 +0000

    using toggle hide

--- public/js/script.js
+++ public/js/script.js
@@ -79,7 +79,7 @@ $(document).mouseup(e => {
    if (!$menu.is(e.target) // if the target of the click isn't the container...
    && $menu.has(e.target).length === 0) // ... nor a descendant of the container
    {
-     $menu.toggleClass('close');
+     $menu.toggleClass('hide');
   }
  });
 
