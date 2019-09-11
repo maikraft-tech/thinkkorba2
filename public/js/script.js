ab8bd7887ba3ea95d21fa46ef3f46e0a1299831e
Korkor <rejoicehormeku@gmail.com>
2019-09-11 19:04:18 +0000

    close

--- public/js/script.js
+++ public/js/script.js
@@ -73,18 +73,18 @@ $(".close").click(function(){
 //     $("#menu").toggle();
 //   }
 // })
-const $menu = $('.dropdown');
+const $menu = $('.menu-bar');
 
 $(document).mouseup(e => {
    if (!$menu.is(e.target) // if the target of the click isn't the container...
    && $menu.has(e.target).length === 0) // ... nor a descendant of the container
    {
-     $menu.removeClass('is-active');
+    $("#menu").toggle();
   }
  });
 
 $('.toggle').on('click', () => {
-  $menu.toggleClass('is-active');
+    $("#menu").toggle();
 });
     $(".menu-bar").click(function(){
         $("#menu").toggle()});
