5f6758a9905d33cad4b6fef238e57a83fcf8e32f
Korkor <rejoicehormeku@gmail.com>
2019-09-11 19:46:44 +0000

    add condition for hasclass ope

--- public/js/script.js
+++ public/js/script.js
@@ -77,9 +77,9 @@ const $menu = $('.menu-bar');
 
 $(document).mouseup(e => {
    if (!$menu.is(e.target) // if the target of the click isn't the container...
-   && $menu.has(e.target).length === 0) // ... nor a descendant of the container
+   && $menu.has(e.target).length === 0 && $menu.hasClass('open')) // ... nor a descendant of the container
    {
-     $menu.removeClass('open');
+     $menu.addClass('close');
   }
  });
 
