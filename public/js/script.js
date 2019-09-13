992529e971013f272cae551e93e255b69710e6f8
Korkor <rejoicehormeku@gmail.com>
2019-09-13 14:06:01 +0000

    commit

--- public/js/script.js
+++ public/js/script.js
@@ -79,12 +79,12 @@ $(document).mouseup(e => {
    if (!$menu.is(e.target) // if the target of the click isn't the container...
    && $menu.has(e.target).length === 0) // ... nor a descendant of the container
    {
-    $("#menu").toggle('open');
+    $("#menu").display = "block";
   }
  });
 
 $('.toggle').on('click', () => {
-    $("#menu").display = "block";
+    $("#menu").toggle();
 });
     $(".menu-bar").click(function(){
         $("#menu").toggle()});
