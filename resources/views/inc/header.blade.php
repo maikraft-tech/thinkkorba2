8de92c1410a6ab18efc1b5695c296b1bfae09f61
Korkor <rejoicehormeku@gmail.com>
2019-09-13 18:41:38 +0000

    home icons taken

--- resources/views/inc/header.blade.php
+++ resources/views/inc/header.blade.php
@@ -11,8 +11,8 @@
                     </div>
                     <div class="col-md-6 col-sm-6 col-xs-6 hidden-xs col-lg-6" style="margin-top: 10px; text-align: center;">
                         <div class="row grid-divider">
-                            <a href="https://liveweb.korbaweb.com/" target="_blank" style="text-decoration: none;"><img src="img/logo/user.png" style="width: 25px; padding-bottom:10px;"><span style="border-right: 2px solid red; margin-right: 10px; padding-right: 15px; padding-left: 5px; color: #61646b; font-size: 15px; font-weight: bold;">User Login</span></a>
-                            <a href="https://livepartner.korbaweb.com/" target="_blank" style="text-decoration: none;"><span style="color: red;font-size: 15px;  padding-left: 3px;font-weight: bold;">Partner Login</span> <img src="img/logo/partner.png" style="width: 30px; padding-bottom:10px;"></a>
+                            <a href="https://liveweb.korbaweb.com/" target="_blank" style="text-decoration: none;"><span style="border-right: 2px solid red; margin-right: 10px; padding-right: 15px; padding-left: 5px; color: #61646b; font-size: 17px; font-weight: bold;">User Login</span></a>
+                            <a href="https://livepartner.korbaweb.com/" target="_blank" style="text-decoration: none;"><span style="color: red;font-size: 17px;  padding-left: 3px;font-weight: bold;">Partner Login</span></a>
                         </div>
                         
                     </div>
