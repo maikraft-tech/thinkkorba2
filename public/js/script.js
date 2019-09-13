feb8e9e588391f935c1b64585108481c9e7d8c43
Korkor <rejoicehormeku@gmail.com>
2019-09-13 14:02:16 +0000

    display block

--- public/js/script.js
+++ public/js/script.js
@@ -76,15 +76,15 @@ $(".close").click(function(){
 const $menu = $('.menu-bar');
 
 $(document).mouseup(e => {
-   if ($menu.is(e.target) // if the target of the click isn't the container...
-   && $menu.has(e.target).length === 1) // ... nor a descendant of the container
+   if (!$menu.is(e.target) // if the target of the click isn't the container...
+   && $menu.has(e.target).length === 0) // ... nor a descendant of the container
    {
     $("#menu").toggle('open');
   }
  });
 
 $('.toggle').on('click', () => {
-    $("#menu").toggle();
+    $("#menu").display = "block";
 });
     $(".menu-bar").click(function(){
         $("#menu").toggle()});
