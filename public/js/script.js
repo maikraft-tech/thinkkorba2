e2d996a72d4ad23a76b0dbaa736d0a0b09ce4716
Korkor <rejoicehormeku@gmail.com>
2019-09-13 09:19:32 +0000

    major changes

--- public/js/script.js
+++ public/js/script.js
@@ -79,12 +79,11 @@ $(document).mouseup(e => {
    if (!$menu.is(e.target) // if the target of the click isn't the container...
    && $menu.has(e.target).length === 1) // ... nor a descendant of the container
    {
-    $("#menu").toggle('open');
-  }
+    $menu.removeClass('is-active');  }
  });
 
 $('.toggle').on('click', () => {
-    $("#menu").toggle();
+    $menu.toggleClass('is-active');
 });
     $(".menu-bar").click(function(){
         $("#menu").toggle()});
