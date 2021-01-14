e714babc83c73228e3a33158f8703c783cf6c161
Rejoice Hormeku <rejoicehormeku@gmail.com>
2021-01-14 16:20:03 +0000

    verification added to head

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,6 +3,7 @@
 <html lang="en">
 
 <head>
+    <meta name="google-site-verification" content="Ii6yb8DHsz6kC33k2rsJrd8rBSLaLs8-SUWms6zu8ao" />
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
    
